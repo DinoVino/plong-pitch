a51d6677e48ec1d91fea12bc3d3898ca21c66e08
Donovan Khoun <donovankhoun@gmail.com>
2024-06-19 22:20:57 +0200

    Added webcam
    Things to implement:
    - Using the webcam feed for the ML processing
    - Configure the ML processing as the logic layer, access it using an API
      endpoint?

--- plong/pitch/static/script.js
+++ plong/pitch/static/script.js
@@ -1,11 +1,31 @@
 console.log("hello world")
 
 function playAudio(){
-  
   var audio = document.getElementById('river');
   console.log(audio.duration);
   audio.volume = 0;
   var maxVolume = 0.1;
+  // Rewrite this nested function to a dedicated piece of code for beauty
   setInterval(function() {if(audio.volume < maxVolume){ audio.volume = audio.volume + 0.01; console.log(audio.volume); audio.play();}},250) 
   audio.loop = true;
+  
+  let feed = document.getElementById("feed");
+  let mediaDevices = navigator.mediaDevices;
+  feed.muted = true; 
+
+  mediaDevices
+    .getUserMedia({
+       video: true,
+       audio: false,
+     })
+    .then((stream) =>{
+      feed.srcObject = stream;
+      feed.addEventListener("loadedmetadata",() =>{
+        feed.play();
+      });
+    })
+    .catch(alert);
+
 }
+
+
