e76f7985673dfa048970a4839a913092d95c995a
Donovan Khoun <donovankhoun@gmail.com>
2024-06-19 19:14:14 +0200

    Added the new visuals The visuals + audio have been added. It works now

--- plong/pitch/static/script.js
+++ plong/pitch/static/script.js
@@ -1,2 +1,11 @@
- console.log("hello world")
+console.log("hello world")
 
+function playAudio(){
+  
+  var audio = document.getElementById('river');
+  console.log(audio.duration);
+  audio.volume = 0;
+  var maxVolume = 0.1;
+  setInterval(function() {if(audio.volume < maxVolume){ audio.volume = audio.volume + 0.01; console.log(audio.volume); audio.play();}},250) 
+  audio.loop = true;
+}
