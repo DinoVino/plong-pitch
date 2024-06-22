d83e63cb66853be93411d9df44b66ae2fada3a93
Donovan Khoun <donovankhoun@gmail.com>
2024-06-22 22:29:45 +0200

    Webcam AI integration realised
    Integrated the webcam feed into the ML algorithm.
    Emotions are now being read in realtime using the MediaStreamProcessor
    from the W3C API.
    
    This method ensures that everything can and will run locally.

--- plong/pitch/analyser.py
+++ plong/pitch/analyser.py
@@ -1,17 +1,51 @@
+import os
 import cv2
 import threading
+import numpy as np
 from deepface import DeepFace
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.exceptions import StopConsumer
 
 # This class should be rewritten to be the consumer. 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + './static/machine-learning/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-class AnalyzerConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+import asyncio
+class AnalyzerConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.loop = asyncio.get_running_loop()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        pass
+    async def disconnect(self, close_code):
+        self.stop = True
+        raise StopConsumer
 
-    def receive(self, text_data):
-        self.send("Nice" + text_data)
+    async def receive(self, bytes_data):
+        if not (bytes_data):
+            print("Closed Connection")
+            await self.close()
+        else:
+            self.frame = await self.loop.run_in_executor(None, cv2.imdecode, np.frombuffer(bytes_data, dtype=np.uint8),cv2.IMREAD_COLOR)
+            self.gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+            self.rgb = cv2.cvtColor(self.gray, cv2.COLOR_GRAY2RGB)
+
+            self.result = "temp"
+
+            # Detect faces in the frame
+            faces = face_cascade.detectMultiScale(self.gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+            for (x, y, w, h) in faces:
+                # Extract the face ROI (Region of Interest)
+                face_roi = self.rgb[y:y + h, x:x + w]
+
+                # Perform emotion analysis on the face ROI
+                result = DeepFace.analyze(face_roi, actions=['emotion'], enforce_detection=False)
+
+                # Determine the dominant emotion
+                emotion = result[0]['dominant_emotion']
+                asyncio.sleep(1000)
+                await self.send(emotion)       
+        
+            
+        
+
+
--- plong/pitch/static/script.js
+++ plong/pitch/static/script.js
@@ -6,6 +6,9 @@ var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
 const ws = new WebSocket(
   ws_scheme + '://' + window.location.host + "/"
 );
+const frames = [];
+const canvas = document.getElementById("TempImage");
+const context = canvas.getContext("2d");
 
 function Mode(){
   if (mode == true){
@@ -16,7 +19,7 @@ function Mode(){
     };
 
     ws.onmessage = (event) => {
-      console.log("WS data received" + event.data);
+      console.log(event.data);
     };
 
     ws.onclose = (event) => {
@@ -29,7 +32,7 @@ function TestMessage(){
   ws.send("hello world");
 }
 
-function playAudio(){
+async function playAudio(){
   var audio = document.getElementById('river');
   console.log(audio.duration);
   audio.volume = 0;
@@ -37,27 +40,85 @@ function playAudio(){
   // Rewrite this nested function to a dedicated piece of code for beauty
   setInterval(function() {if(audio.volume < maxVolume){ audio.volume = audio.volume + 0.01; console.log(audio.volume); audio.play();}},250) 
   audio.loop = true;
+  
   // Code might be obsolete after websocket/django channels implementation  
-  /*
-  let feed = document.getElementById("feed");
-  let mediaDevices = navigator.mediaDevices;
-  feed.muted = true; 
+  var canvas = document.getElementById("TempImage");
+  var context = canvas.getContext("2d");
+  var feed = document.getElementById("feed");
+  // var mediaDevices = navigator.mediaDevices;
+  // feed.muted = true; 
 
-  mediaDevices
-    .getUserMedia({
-       video: true,
-       audio: false,
-     })
-    .then((stream) =>{
-      feed.srcObject = stream;
-      feed.addEventListener("loadedmetadata",() =>{
-        feed.play();
-      });
-    })
-    .catch(alert);
-  */
+  // mediaDevices
+  //   .getUserMedia({
+  //      video: true,
+  //      audio: false,
+  //    })
+  //   .then((stream) =>{
+  //     feed.srcObject = stream;
+  //     feed.play();
+  //     var width = feed.width;
+  //     var height = feed.height;
+  //     var delay = 250;
+  //     var quality = 0.7;
+
+  //     // rewrite the code, blob is empty due to HTML5 changes
+  //     setInterval(() => {
+  //       context.drawImage(feed, 0, 0, width, height);
+  //       canvas.toBlob((blob) =>{
+  //         if(ws.readyState == WebSocket.OPEN){
+  //           if(mode ==true){
+  //             ws.send(new Uint8Array([]));
+  //           }else{
+  //             ws.send(blob);
+  //           }
+  //         }
+  //       }, 'image/jpeg', quality)
+  //     }, delay);
+  //   })
+  //   .catch(alert);
+  await readVideo();
   Mode();
 
 }
+
+async function readVideo()
+{
+  const webcamVideoTag = document.getElementById('feed');
+  navigator.mediaDevices.getUserMedia(
+    {video:true,}
+  )
+  .then((stream) =>{
+    webcamVideoTag.srcObject = stream;
+    webcamVideoTag.play();
+    const videoTrack = stream.getVideoTracks()[0];
+    const trackProcessor = new MediaStreamTrackProcessor({ track: videoTrack });
+    const quality = 0.7;
+    const reader = trackProcessor.readable.getReader();
+    img = new ImageCapture(videoTrack);
+    setInterval(() => {
+      img.grabFrame()
+      .then((imageBitmap) =>{
+      const canvas = document.getElementById("TempImage");
+      context.drawImage(imageBitmap,0,0,700,480);
+      canvas.toBlob((blob) =>{
+          if(ws.readyState == WebSocket.OPEN){
+            if(mode ==true){
+              ws.send(new Uint8Array([]));
+            }else{
+              ws.send(blob);
+            }
+          }
+        }, 'image/jpeg', quality)
+      });
+    },500); 
+    
+
+    });
+
+}
+
+  
+  
+
 
 
